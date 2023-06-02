d5f944eda3ea976fd41708a4f5a2b66947762de2
trimscash <42578480+trimscash@users.noreply.github.com>
2023-06-02 13:41:46 +0900

    Update Footer.tsx

--- src/view/components/Footer.tsx
+++ src/view/components/Footer.tsx
@@ -4,7 +4,7 @@ function Footer() {
   return (
     <div id="footer">
       <div className="cash">
-        <a href="https://github.com/trimscash/akeome/">ソースコード/</a>
+        <a href="https://github.com/trimscash/akeome-react/">ソースコード/</a>
         <a href="https://twitter.com/trims_cash">@trims_cash</a>
         <span style={{ fontSize: 1 + 'em' }}>←誤り等</span>
       </div>
